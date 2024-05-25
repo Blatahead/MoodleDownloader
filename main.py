88fe5b4407930914b76cd93f385dc8e24429efa7
Blatahead <81589270+Blatahead@users.noreply.github.com>
2024-05-26 01:15:59 +0200

    all imports + files and directories

---
+++ main.py
@@ -0,0 +1,6 @@
+from config.config import OUTPUT_DIR
+from modules.authentication import login
+from modules.course_scraper import get_courses
+from modules.file_downloader import download_files
+from utils.helpers import create_directory
+
