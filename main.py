c26e839e42fbc6be4d1c72355895be63ebc128c2
Blatahead <81589270+Blatahead@users.noreply.github.com>
2024-05-28 01:01:21 +0200

    count & browse dashboard cards

--- main.py
+++ main.py
@@ -21,10 +21,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
 from webdriver_manager.chrome import ChromeDriverManager
 from modules.authentication import authenticate
 from modules.counter import count_elements
 from config.config import MOODLE_EMAIL, MOODLE_PASSWORD
+import time
 
 # Use Chrome
 options = Options()
@@ -32,13 +35,22 @@ options = Options()
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 try:
-    authenticate(driver, MOODLE_EMAIL, MOODLE_PASSWORD)
+	authenticate(driver, MOODLE_EMAIL, MOODLE_PASSWORD)
 
-	# Find element by path
-    selector = '/html/body/div[3]/div[5]/div/div[2]/div/section/div/aside/section[1]/div/div/div[1]/div[2]/div/div/div[1]/div/div/div'
-    count = count_elements(driver, selector)
-    print(f"Nombre d'éléments: {count}")
+	# Count number of element by path
+	selector = '/html/body/div[3]/div[5]/div/div[2]/div/section/div/aside/section[1]/div/div/div[1]/div[2]/div/div/div[1]/div/div/div'
+	count = count_elements(driver, selector)
+	
+	if count > 0:
+		# browse all dashboard cards
+		for i in range(1, count+1):
+			dashboardCardText = driver.find_element(By.XPATH, f'/html/body/div[3]/div[5]/div/div[2]/div/section/div/aside/section[1]/div/div/div[1]/div[2]/div/div/div[1]/div/div/div[{i}]/div[1]/div/div/a/span[3]')
+			dashboardCardText.click()
+			time.sleep(4)
+			driver.back()
+			time.sleep(3)
+	else:
+		print("Aucun élément trouvé.")
 
 finally:
     driver.quit()
-
