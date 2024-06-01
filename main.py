2afe8c60566bb3370707f06fca1a37f0ef781596
leoniechap <leonielattes@gmail.com>
2024-06-01 19:41:45 +0200

    com l7 l8

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+#from selenium.webdriver.support import expected_conditions as EC
+#from selenium.common.exceptions import TimeoutException
 import time
 
 #personnal
